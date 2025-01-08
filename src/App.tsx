f6819e8e262e4b7c76ed7ede8d2e29e8d77835ce
Mostafa Khaled <m.khaled201774@gmail.com>
2025-01-08 16:41:23 +0200

    first upload

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Hero from "./Components/Hero";
+import About from "./Components/About";
+import Navbar from "./Components/Navbar";
+import Features from "./Components/Features";
+import Story from "./Components/Story";
+import ContactUs from "./Components/ContactUs";
+import Footer from "./Components/Footer";
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <ContactUs />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
